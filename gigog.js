c38c1b90715de4f5718dc8ce6f3cecd74fdbbd71
Cybersport <Cybersport@26kadr.internal>
2024-05-28 15:59:58 +0300

    сделали 8 заданий

---
+++ gigog.js
@@ -0,0 +1,112 @@
+//Задание №1
+// let sign;
+// sign = prompt('Введите знак +, -, * или /')
+// if (sign == '+'){
+//     let functionPlus;
+//     plus1 = prompt('Введите первое число');
+//     plus2 = prompt('Введите второе число');
+//     plus1 = Number(plus1);
+//     plus2 = Number(plus2);
+//     functionPlus = plus1 + plus2;
+//     alert(functionPlus);
+// }else if (sign == '-'){
+//     let functionMinus;
+//     minus1 = prompt('Введите первое число');
+//     minus2 = prompt('Введите второе число');
+//     minus1 = Number(minus1);
+//     minus2 = Number(minus2);
+//     functionMinus = minus1 - minus2;
+//     alert(functionMinus);   
+// } else if (sign == '*'){
+//     let functionMultiply;
+//     multiply1 = prompt('Введите первое число');
+//     multiply2 = prompt('Введите второе число');
+//     multiply1 = Number(multiply1);
+//     multiply2 = Number(multiply2);
+//     functionMultiply = multiply1 * multiply2;
+//     alert(functionMultiply);
+// }else if(sign == '/'){
+//     let functionDivide;
+//     divide1 = prompt('Введите первое число');
+//     divide2 = prompt('Введите второе число');
+//     divide1 = Number(divide1);
+//     divide2 = Number(divide2);
+//     functionDivide = divide1 / divide2;
+//     alert(functionDivide);
+// } else {
+//     alert('Введен неправильный знак')
+// }
+// Задание 2
+// function plus(text){
+//     return text.length;
+// } 
+// text = prompt('Введите что нибудь')
+// alert(plus(text))
+
+
+// Задание 3
+// let a;
+// a = prompt('Введите число')
+// if (a > 0){
+//     alert('Число положительное')
+// } else if (a < 0){
+//     alert('Число отрицательное')
+// } else if (a == 0){
+//     alert('Число нулевое')
+// } else{
+//     alert('Это не число')
+// }
+
+// Задание 4
+// let d = 3;
+// function plus(a, b, c){
+//     return (a + b + c)/ d
+// }
+// a = prompt('Введите первое число')
+// b = prompt('Введите второе число')
+// c = prompt('Введите третье число')
+// a = Number(a)
+// b = Number(b)
+// c = Number(c)
+// alert(plus(a, b, c))
+
+// Задание 5
+// let a = {
+//     name: 'ivan',
+//     age: 50
+// }
+// a.name = prompt('Введите имя')
+// a.age = prompt('Введите возраст')
+// alert(`Имя: ${a.name} \nВозраст: ${a.age}`)
+
+// Задание 6
+//  let a;
+//  a = prompt('Введите число')
+//  a = Number(a)
+//  if (a % 2){
+//      alert('Число нечетное')
+//  } else {
+//      alert('Число четное')
+//  }
+
+// Задание 7
+// let a;
+// a = prompt('Введите число')
+// if (a < 0){
+//     alert('Число положительное')
+// } else{
+//     alert('Число не отрицательное')
+// }
+
+// Задание 8
+// let a;
+// let b;
+// a = prompt('Введите первое число')
+// b = prompt('Введите второе число')
+// a = Number(a)
+// b = Number(b)
+// if (a % b){
+//     alert('Число не делится без остатка')
+// } else {
+//     alert('Число делится без остатка')
+// }
